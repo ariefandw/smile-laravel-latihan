d4b4380593d507d41b63fe1f825635060ee2309c
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2022-06-21 08:51:43 +0700

    ganti ke bootststrap

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,14 +1,13 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Post') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
                     <form method="get" action="{{ route('post.index') }}">
                         <input type="text" name="q" value="{{ $q }}"><button type="submit">Cari</button>
                     </form>
@@ -44,4 +43,5 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</div>
+@endsection
